3f15b9b19c87096e32521a0f82cd3f3b95a3c919
osotreo <alfredosuspes@gmail.com>
2022-06-16 20:15:28 -0500

    cambio

---
+++ app/src/main/java/com/example/urgencs/hospital.kt
@@ -0,0 +1,32 @@
+package com.example.urgencs
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+
+
+
+
+   // class hospital: AppCompatActivity() {
+
+       // override fun onCreate(savedInstanceState: Bundle?) {
+      //      super.onCreate(savedInstanceState)
+            //   setContentView(R.layout.hospital)
+            //info
+            //    val txt_nombre=findViewById<EditText>(R.id.text_nombre)
+            //   val txt_codigo=findViewById<EditText>(R.id.text_codigo)
+            //  val txt_accidente=findViewById<EditText>(R.id.text_accidente)
+
+
+            //  boton_agregar.setOnClickListener {
+        //    try {
+         //       val codigo = txt_codigo.text.toString().toInt()
+            //    val nombre = txt_nombre.text.toString()
+         //       val accidente = txt_autor.text.toString()
+
+
+        //    }
+   //     }
