bd3df4999ef0b51ec122f353c76a2cfb437f7d58
Andrés Siachoque <andressiachoque96@hotmail.com>
2022-06-17 19:18:14 -0500

    Diseño menú principal y menú hospital interactundo entre vistas

--- app/src/main/java/com/example/urgencs/MainActivity.kt
+++ app/src/main/java/com/example/urgencs/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.urgencs
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bn_hospital_main=findViewById<AppCompatButton>(R.id.bn_hospital_main)
+        bn_hospital_main.setOnClickListener {
+            val intent= Intent(this,HospitalActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
-fun main(){}
